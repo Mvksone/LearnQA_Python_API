0adaada012805453242b53b101877114fc4dd9e7
Mvksone <skubb.m@gmail.com>
2023-08-22 10:59:22 +0300

    update ex6_long_redirect.py

--- ex6_long_redirect.py
+++ ex6_long_redirect.py
@@ -2,8 +2,10 @@ from json.decoder import JSONDecodeError
 import requests
 
 response = requests.get("https://playground.learnqa.ru/api/long_redirect", allow_redirects=True)
-first_responce = response.history[0]
-second_responce = response
+hist = response.history
 
-print(first_responce.url)
-print(second_responce.url)
+for i in range(len(hist)):
+    print(hist[i].url)
+
+finish_responce = response
+print(finish_responce.url)
