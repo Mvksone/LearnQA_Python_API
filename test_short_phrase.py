329ee4e89ad1dd0a5e004ebe30307e4b241f869f
Mvksone <skubb.m@gmail.com>
2023-08-29 15:00:21 +0300

    add ex10 test_short_phrase.py

---
+++ test_short_phrase.py
@@ -0,0 +1,10 @@
+import pytest
+
+class TestShortPhrase:
+    def test_short_phrase(self):
+        phrase = input("Set a phrase ")
+
+        expected_max_phrase_lenght = 15
+        actual_phrase_lenght = len(phrase)
+
+        assert expected_max_phrase_lenght > actual_phrase_lenght, "Actual phrase lenght is longer than expected"
