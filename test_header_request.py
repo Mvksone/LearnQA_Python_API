82802644ec46ad95693195252a890c84fe405cf2
Mvksone <skubb.m@gmail.com>
2023-08-29 16:32:08 +0300

    add ex12 test_header_request.py_request.py.py

---
+++ test_header_request.py
@@ -0,0 +1,24 @@
+import pytest
+import requests
+
+class TestHeaderRequest:
+
+    def test_request_header(self):
+        response = requests.get("https://playground.learnqa.ru/api/homework_header")
+        headers = response.headers
+        print(headers)
+
+        if headers:
+            actual_header_name = list(headers.keys())[6]
+            actual_header_value = headers.get(actual_header_name)
+            print('Header Name is ', actual_header_name)
+            print('Header value is ', actual_header_value)
+
+            expected_header_name = "x-secret-homework-header"
+            expected_header_value = 'Some secret value'
+
+            assert expected_header_name == actual_header_name
+            assert expected_header_value == actual_header_value
+        else:
+            print("Headers not found")
+
