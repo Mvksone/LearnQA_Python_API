ef4114adcbba611734be885b36cd05faf548b0f9
Mvksone <skubb.m@gmail.com>
2023-09-04 16:37:16 +0300

    add ex. 15 test_user_register.py

---
+++ tests/test_user_register.py
@@ -0,0 +1,121 @@
+import requests
+from lib.base_case import BaseCase
+from lib.assertions import Assertions
+import pytest
+
+class TestUserRegister(BaseCase):
+
+    missing_params = [
+        ("password"),
+        ("username"),
+        ("firstName"),
+        ("lastName"),
+        ("email")
+    ]
+
+    def test_create_user_with_existing_email(self):
+        email = 'vinkotov@example.com'
+        data = {
+            'password': '123',
+            'username': 'learnqa',
+            'firstName': 'learnqa',
+            'lastName': 'learnqa',
+            'email': email
+        }
+
+        response = requests.post("https://playground.learnqa.ru/api/user", data=data)
+
+        assert response.status_code == 400, f"Unexpected status code {response.status_code}"
+        assert response.content.decode("utf-8") == f"Users with email '{email}' already exists", f"Unexpected response content {response.content}"
+
+    def test_create_user_with_incorrect_email(self):
+        email = 'vinkotovexample.com'
+        data = {
+            'password': '123',
+            'username': 'learnqa',
+            'firstName': 'learnqa',
+            'lastName': 'learnqa',
+            'email': email
+        }
+
+        response = requests.post("https://playground.learnqa.ru/api/user", data=data)
+
+        assert response.status_code == 400, f"Unexpected status code {response.status_code}"
+        assert response.content.decode("utf-8") == f"Invalid email format", f"Unexpected response content {response.content}"
+
+    @pytest.mark.parametrize('condition', missing_params)
+    def test_create_user_without_any_param(self, condition):
+        if condition == 'password':
+            data = {
+            'username': 'learnqa',
+            'firstName': 'learnqa',
+            'lastName': 'learnqa',
+            'email': 'vinkotov@example.com'
+            }
+        elif condition == 'username':
+            data = {
+            'password': '123',
+            'firstName': 'learnqa',
+            'lastName': 'learnqa',
+            'email': 'vinkotov@example.com'
+            }
+        elif condition == 'firstName':
+            data = {
+            'password': '123',
+            'username': 'learnqa',
+            'lastName': 'learnqa',
+            'email': 'vinkotov@example.com'
+            }
+        elif condition == 'lastName':
+            data = {
+            'password': '123',
+            'username': 'learnqa',
+            'firstName': 'learnqa',
+            'email': 'vinkotov@example.com'
+            }
+        elif condition == 'email':
+            data = {
+            'password': '123',
+            'username': 'learnqa',
+            'firstName': 'learnqa',
+            'lastName': 'learnqa'
+            }
+
+        response = requests.post("https://playground.learnqa.ru/api/user", data=data)
+
+        assert response.status_code == 400, f"Unexpected status code {response.status_code}"
+        assert response.content.decode("utf-8") == f"The following required params are missed: {condition}", f"Unexpected response content {response.content}"
+
+
+    def test_create_user_with_short_username(self):
+        username = 'a'
+        data = {
+            'password': '123',
+            'username': username,
+            'firstName': 'learnqa',
+            'lastName': 'learnqa',
+            'email': 'vinkotov@example.com'
+        }
+
+        response = requests.post("https://playground.learnqa.ru/api/user", data=data)
+
+        assert response.status_code == 400, f"Unexpected status code {response.status_code}"
+        assert response.content.decode(
+                "utf-8") == f"The value of 'username' field is too short", f"Unexpected response content {response.content}"
+
+    def test_create_user_with_long_username(self):
+        #username of 253 characters
+        username = 'werhazhphsnhvtfsydzlfvpdfcfplbguitheplytwyxevnxhqjlsgtrprfwurhrrnfuvifybtryywqbdddxnzerggxlyzgtxrsbyrvtjzymiicldkrdmqulvafjvebnvcgfnnmlxbyhdtyulctmkvlfhsyqftliszcvuungjltdtwozxcsxdehvwtpctnmrmudxyidtkvwjhrknenxlljsgzgbrgqwtsfvjzgjffhjhcfxxmttgvrkbfnlnyr'
+        data = {
+            'password': '123',
+            'username': username,
+            'firstName': 'learnqa',
+            'lastName': 'learnqa',
+            'email': 'vinkotov@example.com'
+        }
+
+        response = requests.post("https://playground.learnqa.ru/api/user", data=data)
+
+        assert response.status_code == 400, f"Unexpected status code {response.status_code}"
+        assert response.content.decode(
+                "utf-8") == f"The value of 'username' field is too long", f"Unexpected response content {response.content}"
