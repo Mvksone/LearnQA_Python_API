b9fe055e00ec0cff87a2712b3c9e4c72328bde4c
Mvksone <skubb.m@gmail.com>
2023-09-11 13:03:33 +0300

    add allure tags

--- tests/test_user_delete.py
+++ tests/test_user_delete.py
@@ -27,113 +27,111 @@ class TestUserDelete(BaseCase):
         Assertions.assert_code_status(response1, 400)
         assert response1.content.decode("utf-8") == f"Please, do not delete test users with ID 1, 2, 3, 4 or 5.", f"Unexpected response content {response.content}"
 
-    @allure.description("This test deleting a just user")
+    @allure.story("Test deleting a just create user")
     def test_delete_just_create_user(self):
-        # REGISTER
-        register_data = self.prepare_registration_data()
-        response1 = MyRequests.post("/user", data=register_data)
+        with allure.step("Registration user"):
+            register_data = self.prepare_registration_data()
+            response1 = MyRequests.post("/user", data=register_data)
 
-        Assertions.assert_code_status(response1, 200)
-        Assertions.assert_json_has_key(response1, "id")
+            Assertions.assert_code_status(response1, 200)
+            Assertions.assert_json_has_key(response1, "id")
 
-        email = register_data['email']
-        password = register_data['password']
-        user_id = self.get_json_value(response1, "id")
+            email = register_data['email']
+            password = register_data['password']
+            user_id = self.get_json_value(response1, "id")
 
-        # LOGIN
-        login_data = {
-            'email': email,
-            'password': password
-        }
-        response2 = MyRequests.post("/user/login", data=login_data)
+        with allure.step("Login user"):
+            login_data = {
+                'email': email,
+                'password': password
+            }
+            response2 = MyRequests.post("/user/login", data=login_data)
 
-        auth_sid = self.get_cookie(response2, "auth_sid")
-        token = self.get_header(response2, "x-csrf-token")
-        Assertions.assert_code_status(response1, 200)
+            auth_sid = self.get_cookie(response2, "auth_sid")
+            token = self.get_header(response2, "x-csrf-token")
+            Assertions.assert_code_status(response1, 200)
 
-        #DELETE
-        response3 = MyRequests.delete(f"/user/{user_id}",
-                    headers={"x-csrf-token": token},
-                    cookies={"auth_sid": auth_sid}
-                    )
-        Assertions.assert_code_status(response3, 200)
+        with allure.step("Delete just reg user"):
+            response3 = MyRequests.delete(f"/user/{user_id}",
+                        headers={"x-csrf-token": token},
+                        cookies={"auth_sid": auth_sid}
+                        )
+            Assertions.assert_code_status(response3, 200)
 
-        #GET USER DATA
-        response4 = MyRequests.get(
-            f"/user/{user_id}",
-            headers={"x-csrf-token": token},
-            cookies={"auth_sid": auth_sid},
-        )
+        with allure.step("Check user data"):
+            response4 = MyRequests.get(
+                f"/user/{user_id}",
+                headers={"x-csrf-token": token},
+                cookies={"auth_sid": auth_sid},
+            )
 
-        Assertions.assert_code_status(response3, 200)
-        assert response4.content.decode(
-            "utf-8") == f"User not found", f"Unexpected response content {response4.content}"
+            Assertions.assert_code_status(response3, 200)
+            assert response4.content.decode(
+                "utf-8") == f"User not found", f"Unexpected response content {response4.content}"
 
-    @allure.description("Attempt to delete a new user with auth under a another user")
+    @allure.story("Story: Attempt to delete a new user with auth under a another user")
     def test_delete_new_user_with_auth_another_user(self):
-        # REGISTER USER_1
-        register_data = self.prepare_registration_data()
-        response1 = MyRequests.post("/user", data=register_data)
+        with allure.step("Reg user 1"):
+            register_data = self.prepare_registration_data()
+            response1 = MyRequests.post("/user", data=register_data)
 
-        Assertions.assert_code_status(response1, 200)
-        Assertions.assert_json_has_key(response1, "id")
+            Assertions.assert_code_status(response1, 200)
+            Assertions.assert_json_has_key(response1, "id")
 
-        user1_email = register_data['email']
-        user1_password = register_data['password']
-        #user1_id = self.get_json_value(response1, "id")
+            user1_email = register_data['email']
+            user1_password = register_data['password']
+            #user1_id = self.get_json_value(response1, "id")
 
-        # LOGIN NEW USER_1
-        new_user_reg_data = {
-            "email": user1_email,
-            "password": user1_password
-        }
-        response2 = MyRequests.post("/user/login", data=new_user_reg_data)
-        user1_auth_sid = self.get_cookie(response2, "auth_sid")
-        user1_token = self.get_header(response2, "x-csrf-token")
+        with allure.step("Login user 1"):
+            new_user_reg_data = {
+                "email": user1_email,
+                "password": user1_password
+            }
+            response2 = MyRequests.post("/user/login", data=new_user_reg_data)
+            user1_auth_sid = self.get_cookie(response2, "auth_sid")
+            user1_token = self.get_header(response2, "x-csrf-token")
 
+        with allure.step("Reg user 2"):
+            register_data2 = self.prepare_registration_data()
+            response3 = MyRequests.post("/user", data=register_data2)
 
-        # REG NEW USER_2
-        register_data2 = self.prepare_registration_data()
-        response3 = MyRequests.post("/user", data=register_data2)
+            Assertions.assert_code_status(response3, 200)
+            Assertions.assert_json_has_key(response3, "id")
 
-        Assertions.assert_code_status(response3, 200)
-        Assertions.assert_json_has_key(response3, "id")
+            new_user2_email = register_data2['email']
+            new_user2_password = register_data2['password']
+            new_user2_id = self.get_json_value(response3, "id")
 
-        new_user2_email = register_data2['email']
-        new_user2_password = register_data2['password']
-        new_user2_id = self.get_json_value(response3, "id")
+        with allure.step("Delete user 2 with user 1 data"):
+            response4 = MyRequests.delete(
+                f"/user/{new_user2_id}",
+                headers={"x-csrf-token": user1_token},
+                cookies={"auth_sid": user1_auth_sid}
+            )
+            Assertions.assert_code_status(response4, 200)
 
+        with allure.step("Login user 2"):
+            data = {
+                'email': new_user2_email,
+                'password': new_user2_password
+            }
 
-        # DELETE USER_2 WITH USER_1 DATA
-        response4 = MyRequests.delete(
-            f"/user/{new_user2_id}",
-            headers={"x-csrf-token": user1_token},
-            cookies={"auth_sid": user1_auth_sid}
-        )
-        Assertions.assert_code_status(response4, 200)
+            response5 = MyRequests.post("/user/login", data=data)
 
-        # LOGIN USER_2
-        data = {
-            'email': new_user2_email,
-            'password': new_user2_password
-        }
+            new_user2_auth_sid = self.get_cookie(response5, "auth_sid")
+            new_user2_token = self.get_header(response5, "x-csrf-token")
 
-        response5 = MyRequests.post("/user/login", data=data)
-
-        new_user2_auth_sid = self.get_cookie(response5, "auth_sid")
-        new_user2_token = self.get_header(response5, "x-csrf-token")
-
-        # GET USER_2 DATA
-        response6 = MyRequests.get(
-            f"/user/{new_user2_id}",
-            headers={"x-csrf-token": new_user2_token},
-            cookies={"auth_sid": new_user2_auth_sid}
-        )
-        print(response6.content)
-        Assertions.assert_json_has_not_key(response6, 'id')
-        Assertions.assert_json_has_not_key(response6, 'email')
-        Assertions.assert_json_has_not_key(response6, 'firstName')
-        Assertions.assert_json_has_not_key(response6, 'lastName')
+        with allure.step("Check user 2 data"):
+            response6 = MyRequests.get(
+                f"/user/{new_user2_id}",
+                headers={"x-csrf-token": new_user2_token},
+                cookies={"auth_sid": new_user2_auth_sid}
+            )
+            print(response6.content)
+            Assertions.assert_json_has_not_key(response6, 'id')
+            Assertions.assert_json_has_not_key(response6, 'email')
+            Assertions.assert_json_has_not_key(response6, 'firstName')
+            Assertions.assert_json_has_not_key(response6, 'lastName')
 
 
 
--- tests/test_user_edit.py
+++ tests/test_user_edit.py
@@ -67,134 +67,132 @@ class TestUserEdit(BaseCase):
         assert response1.content.decode(
                 "utf-8") == f"Auth token not supplied", f"Unexpected response content"
 
-    @allure.description("This test edit new user with auth as another user")
+    @allure.story("Story: edit a new user with auth as another user")
     def test_edit_new_user_with_auth_another_user(self):
-         # REGISTER USER_1
-         register_data =self.prepare_registration_data()
-         response1 = MyRequests.post("/user", data=register_data)
+         with allure.step("Register user 1"):
+             register_data =self.prepare_registration_data()
+             response1 = MyRequests.post("/user", data=register_data)
 
-         Assertions.assert_code_status(response1, 200)
-         Assertions.assert_json_has_key(response1, "id")
+             Assertions.assert_code_status(response1, 200)
+             Assertions.assert_json_has_key(response1, "id")
 
-         new_user_email = register_data['email']
-         new_user_password = register_data['password']
-
-
-         # LOGIN NEW USER_1
-         new_user_reg_data = {
-             "email": new_user_email,
-             "password": new_user_password
-         }
-         response2 = MyRequests.post("/user/login", data=new_user_reg_data)
-         new_user_auth_sid = self.get_cookie(response2, "auth_sid")
-         new_user_token = self.get_header(response2, "x-csrf-token")
+             new_user_email = register_data['email']
+             new_user_password = register_data['password']
 
 
-         # REG NEW USER_2
-         register_data =self.prepare_registration_data()
-         response3 = MyRequests.post("/user", data=register_data)
+         with allure.step("Login user 1"):
+             new_user_reg_data = {
+                 "email": new_user_email,
+                 "password": new_user_password
+             }
+             response2 = MyRequests.post("/user/login", data=new_user_reg_data)
+             new_user_auth_sid = self.get_cookie(response2, "auth_sid")
+             new_user_token = self.get_header(response2, "x-csrf-token")
 
-         print(response3.content)
-         Assertions.assert_code_status(response3, 200)
-         Assertions.assert_json_has_key(response3, "id")
+         with allure.step("Registration user 2"):
+             register_data =self.prepare_registration_data()
+             response3 = MyRequests.post("/user", data=register_data)
 
-         new_user2_email = register_data['email']
-         new_user2_password = register_data['password']
-         new_user2_id = self.get_json_value(response3, "id")
+             Assertions.assert_code_status(response3, 200)
+             Assertions.assert_json_has_key(response3, "id")
 
-         # EDIT USER_2 WITH USER_1 DATA
-         new_name = "Changed Name"
-         response4 = MyRequests.put(
-             f"/user/{new_user2_id}",
-             headers={"x-csrf-token": new_user_token},
-             cookies={"auth_sid": new_user_auth_sid},
-             data={"firstName": new_name}
-         )
-         Assertions.assert_code_status(response4, 200)
+             new_user2_email = register_data['email']
+             new_user2_password = register_data['password']
+             new_user2_id = self.get_json_value(response3, "id")
 
-         # LOGIN USER_2
-         data ={
-             'email': new_user2_email,
-             'password': new_user2_password
-         }
+         with allure.step("Edit user 2 with user 1 data"):
+             new_name = "Changed Name"
+             response4 = MyRequests.put(
+                 f"/user/{new_user2_id}",
+                 headers={"x-csrf-token": new_user_token},
+                 cookies={"auth_sid": new_user_auth_sid},
+                 data={"firstName": new_name}
+             )
+             Assertions.assert_code_status(response4, 200)
 
-         response5 = MyRequests.post("/user/login", data=data)
-         new_user2_auth_sid = self.get_cookie(response5, "auth_sid")
-         new_user2_token = self.get_header(response5, "x-csrf-token")
+         with allure.step("Login user 2"):
+             data ={
+                 'email': new_user2_email,
+                 'password': new_user2_password
+             }
 
-        # GET USER_2 DATA
-         response6 = MyRequests.get(
-              f"/user/{new_user2_id}",
-              headers={"x-csrf-token": new_user2_token},
-              cookies={"auth_sid": new_user2_auth_sid}
-          )
-         edited_user_email = self.get_json_value(response6, "firstName")
-         assert edited_user_email == new_name, f"The firstName parameter of user2 has not changed"
+             response5 = MyRequests.post("/user/login", data=data)
+             new_user2_auth_sid = self.get_cookie(response5, "auth_sid")
+             new_user2_token = self.get_header(response5, "x-csrf-token")
 
-    @allure.description("This test edit wrong email with auth the same user")
+         with allure.step("Get and assert user 2 data"):
+            response6 = MyRequests.get(
+                  f"/user/{new_user2_id}",
+                  headers={"x-csrf-token": new_user2_token},
+                  cookies={"auth_sid": new_user2_auth_sid}
+            )
+            edited_user_email = self.get_json_value(response6, "firstName")
+            assert edited_user_email == new_name, f"The firstName parameter of user2 has not changed"
+
+    @allure.story("Story: test edit wrong email with auth the same user")
     def test_edit_wrong_email_with_auth_same_user(self):
-    # REGISTER NEW USER
-        register_data =self.prepare_registration_data()
-        response1 = MyRequests.post("/user", data=register_data)
+        with allure.step("Register user 1"):
+            register_data =self.prepare_registration_data()
+            response1 = MyRequests.post("/user", data=register_data)
 
-        Assertions.assert_code_status(response1, 200)
-        Assertions.assert_json_has_key(response1, "id")
+            Assertions.assert_code_status(response1, 200)
+            Assertions.assert_json_has_key(response1, "id")
 
-        email = register_data['email']
-        password = register_data['password']
-        user_id = self.get_json_value(response1, "id")
+            email = register_data['email']
+            password = register_data['password']
+            user_id = self.get_json_value(response1, "id")
 
-    # LOGIN NEW USER 1
-        login_data = {
-            "email": email,
-            "password": password
-        }
-        response2 = MyRequests.post("/user/login", data=login_data)
+        with allure.step("Login user 1"):
+            login_data = {
+                "email": email,
+                "password": password
+            }
+            response2 = MyRequests.post("/user/login", data=login_data)
 
-        auth_sid = self.get_cookie(response2, "auth_sid")
-        token = self.get_header(response2, "x-csrf-token")
+            auth_sid = self.get_cookie(response2, "auth_sid")
+            token = self.get_header(response2, "x-csrf-token")
 
-    # EDIT
-        new_email = "wrongemail"
-        response3 = MyRequests.put(
-            f"/user/{user_id}",
-            headers={"x-csrf-token": token},
-            cookies={"auth_sid": auth_sid},
-            data={"email": new_email}
-        )
-        Assertions.assert_code_status(response3, 400)
-        assert response3.content.decode("utf-8") == f"Invalid email format", f"Unexpected response content {response3.content}"
+        with allure.step("Edit user 1 with wrong email"):
+            new_email = "wrongemail"
+            response3 = MyRequests.put(
+                f"/user/{user_id}",
+                headers={"x-csrf-token": token},
+                cookies={"auth_sid": auth_sid},
+                data={"email": new_email}
+            )
+            Assertions.assert_code_status(response3, 400)
+            assert response3.content.decode("utf-8") == f"Invalid email format", f"Unexpected response content {response3.content}"
 
-    @allure.description("This test edit wrong firstName with auth the same user")
+    @allure.story("Story: test edit wrong firstName with auth the same user")
     def test_edit_wrong_firstName_with_auth_same_user(self):
-    # REGISTER NEW USER
-        register_data =self.prepare_registration_data()
-        response1 = MyRequests.post("/user", data=register_data)
+        with allure.step("Register new user"):
+            register_data =self.prepare_registration_data()
+            response1 = MyRequests.post("/user", data=register_data)
 
-        Assertions.assert_code_status(response1, 200)
-        Assertions.assert_json_has_key(response1, "id")
+            Assertions.assert_code_status(response1, 200)
+            Assertions.assert_json_has_key(response1, "id")
 
-        email = register_data['email']
-        password = register_data['password']
-        user_id = self.get_json_value(response1, "id")
+            email = register_data['email']
+            password = register_data['password']
+            user_id = self.get_json_value(response1, "id")
 
-    # LOGIN NEW USER 1
-        login_data = {
-            "email": email,
-            "password": password
-        }
-        response2 = MyRequests.post("/user/login", data=login_data)
-        auth_sid = self.get_cookie(response2, "auth_sid")
-        token = self.get_header(response2, "x-csrf-token")
+        with allure.step("Login user"):
+            login_data = {
+                "email": email,
+                "password": password
+            }
+            response2 = MyRequests.post("/user/login", data=login_data)
+            auth_sid = self.get_cookie(response2, "auth_sid")
+            token = self.get_header(response2, "x-csrf-token")
 
-    # EDIT
-        new_name = "a"
-        response3 = MyRequests.put(
-            f"/user/{user_id}",
-            headers={"x-csrf-token": token},
-            cookies={"auth_sid": auth_sid},
-            data={"firstName": new_name}
-        )
-        Assertions.assert_code_status(response3, 400)
-        error = self.get_json_value(response3, "error")
-        assert error == f"Too short value for field firstName", f"Unexpected response content {response3.content}"
+        with allure.step("Edit user with wrong short firstName"):
+            new_name = "a"
+            response3 = MyRequests.put(
+                f"/user/{user_id}",
+                headers={"x-csrf-token": token},
+                cookies={"auth_sid": auth_sid},
+                data={"firstName": new_name}
+            )
+            Assertions.assert_code_status(response3, 400)
+            error = self.get_json_value(response3, "error")
+            assert error == f"Too short value for field firstName", f"Unexpected response content {response3.content}"
